59b38c95186582faac20a3a9fdbcf68730ca5256
Shubhamt31 <86181994+Shubhamt31@users.noreply.github.com>
2023-12-11 10:35:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// Replace 'YOUR_API_KEY' with your actual YouTube API key
-const apiKey = 'AIzaSyA46IcNz2ZbijNK1LLs0bU4r__lC0PPQ2s';
-const videoId = 'Kl46_z65ypU';
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = 'AIzaSyA46IcNz2ZbijNK1LLs0bU4r__lC0PPQ2s';
+    const videoId = 'Kl46_z65ypU';
 
-// Fetch video details using YouTube Data API
-fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-        const videoTitle = data.items[0].snippet.title;
-        const videoDescription = data.items[0].snippet.description;
-        const videoPlayer = document.getElementById('youtube-player');
+    const videoPlayer = document.getElementById('custom-player');
+    const playButton = document.getElementById('play-button');
 
-        // Set video player attributes
-        videoPlayer.src = `https://www.youtube.com/embed/${videoId}`;
-        videoPlayer.title = videoTitle;
-        videoPlayer.allow = 'autoplay; encrypted-media; picture-in-picture';
-    })
-    .catch(error => console.error('Error fetching video details:', error));
+    playButton.addEventListener('click', function () {
+        // Set video source dynamically
+        videoPlayer.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+        videoPlayer.style.display = 'block';
+        playButton.style.display = 'none';
+    });
+});
